bd2184f061f154f251ba822504be3148678fb21e
Guy haiby <ghaiby@ucsc.edu>
2024-11-07 13:35:10 -0800

    small change: message button

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 const app = document.querySelector<HTMLDivElement>("#app")!;
-//message button
 const messageButton = document.createElement("button");
 app.appendChild(messageButton);
 messageButton.innerText = "message";
