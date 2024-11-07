35899ef7da2659a668c7c15f710a68c7532e8d6b
Guy haiby <ghaiby@ucsc.edu>
2024-11-07 13:30:09 -0800

    small change: message button

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-// todo
+const app = document.querySelector<HTMLDivElement>("#app")!;
+const messageButton = document.createElement("button");
+app.appendChild(messageButton);
+messageButton.innerText = "message";
+function handleClick(): void {
+  alert("Hello");
+}
+messageButton.addEventListener("click", handleClick);
